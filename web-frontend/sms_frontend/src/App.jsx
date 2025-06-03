8abbd33cef57ebc5547c1ba5610eca3ba573d8af
Shubhamashish1310 <98949989+Shubhamashish1310@users.noreply.github.com>
2025-06-03 14:57:00 +0530

    basic setup react and daisy UI

---
+++ web-frontend/sms_frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='bg-blue-200'>App
+    <button class="btn btn-primary">Button</button>
+    </div>
+  )
+}
+
+export default App
